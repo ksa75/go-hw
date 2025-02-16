08d3a4a65ddd4f93adaa45a5950a590c63f8d946
ksa75 <kirillov-sa@yandex.ru>
2025-02-17 01:25:45 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -19,18 +19,11 @@ func Unpack(input string) (string, error) {
 	var result strings.Builder
 
 	var prevCh, nextCh rune
-	var nextZero, escape, escaped, prevEscaped bool
+	var escape, escaped, prevEscaped bool
 	i := 0
 	for i < len(input) {
 		ch, size := utf8.DecodeRuneInString(input[i:])
 		nextCh, _ = utf8.DecodeRuneInString(input[i+size:])
-
-		// проверка следущего на ноль
-		if unicode.IsDigit(nextCh) {
-			if nextCh == '0' {
-				nextZero = true
-			}
-		}
 
 		// сейчас у нас экран
 		if ch == '\\' {
@@ -38,19 +31,22 @@ func Unpack(input string) (string, error) {
 				return "", fmt.Errorf("неправильное экранирование: %w", ErrInvalidString)
 			}
 			escape = true
+			escaped = false
+			prevEscaped = false
 		}
 
 		// символ заэкранирован
 		if escape && isEscapable(ch) {
-			escaped = true
 			escape = false
+			escaped = true
+			prevEscaped = false
 		}
 
 		// предыдущий символ заэкранирован
 		if escaped && unicode.IsDigit(prevCh) && unicode.IsDigit(ch) {
-			prevEscaped = true
 			escape = false
 			escaped = false
+			prevEscaped = true
 		}
 
 		// обработка числа
@@ -63,17 +59,17 @@ func Unpack(input string) (string, error) {
 				return "", fmt.Errorf("неправильное количество: %w", ErrInvalidString)
 			}
 
-			if repeatCount, _ := strconv.Atoi(string(ch)); repeatCount != 0 {
-				if escaped {
-					// добавляем если экранировано и следущее не ноль
-					if !nextZero {
-						result.WriteString(strings.Repeat(string(ch), 1))
-					}
-				} else {
-					// добиваем по счетчику
+			repeatCount, _ := strconv.Atoi(string(ch))
+			if escaped {
+				// добавляем если экранировано и следущее не ноль
+				if nextCh != '0' {
+					result.WriteString(strings.Repeat(string(ch), 1))
+				}
+			} else {
+				// добиваем по счетчику
+				if repeatCount > 0 {
 					result.WriteString(strings.Repeat(string(prevCh), repeatCount-1))
 				}
-				prevEscaped = false
 			}
 			i++
 			prevCh = ch
@@ -81,16 +77,11 @@ func Unpack(input string) (string, error) {
 		}
 
 		// добавляем если не экран и следущее не ноль
-		if !nextZero && !escape {
+		if nextCh != '0' && !escape {
 			result.WriteRune(ch)
 		}
-
-		nextZero = false
 		prevCh = ch
 		i += size
-		if length := utf8.RuneCount([]byte(input)); i > length {
-			break
-		}
 	}
 
 	return result.String(), nil
