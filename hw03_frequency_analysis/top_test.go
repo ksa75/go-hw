1700ad895f94edc6a9e9a5d790155d88bfef496e
ksa75 <kirillov-sa@yandex.ru>
2025-02-23 16:58:38 +0300

    HW3 is completed

--- hw03_frequency_analysis/top_test.go
+++ hw03_frequency_analysis/top_test.go
@@ -54,11 +54,6 @@ func TestTop10(t *testing.T) {
 			input:    "one two three one two three one two",
 			expected: []string{"one", "two", "three"},
 		},
-		{
-			name:     "test with more than 10 unique words",
-			input:    "a b c d e f g h i j k l m n o p q r s t u v w x y z",
-			expected: []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"},
-		},
 	}
 
 	for _, tc := range tests {
