3539eff29d21d280cc3a87c0bc18989e8e6ea0a7
ksa75 <kirillov-sa@yandex.ru>
2025-03-23 21:48:40 +0300

    HW6 is completed

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,36 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	ch := in
+	for _, stage := range stages {
+		ch = stage(readCh(ch, done))
+	}
+	return ch
 }
+
+func readCh(in In, done In) Out {
+	nextCh := make(Bi)
+	go func() {
+		defer close(nextCh)
+		for {
+			select {
+			case <-done:
+				<-in // Вычитываем из канала значение чтобы разблокировать писателя
+				return
+			case data, ok := <-in:
+				if !ok {
+					return
+				}
+				select {
+				case <-done: // Проверка done перед отправкой
+				case nextCh <- data:
+				}
+			}
+		}
+	}()
+	return nextCh
+}
+
+// 			result := stages[3](stages[2](stages[1](stages[0](in))))
+// 			return result
+// }
