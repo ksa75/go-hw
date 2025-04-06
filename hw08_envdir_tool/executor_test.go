0d3dc23b31bff103c183541c8138bee52e7b44e2
ksa75 <kirillov-sa@yandex.ru>
2025-04-06 21:45:07 +0300

    HW8 is completed

--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -1,12 +1,50 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+	"testing"
+)
 
 func TestRunCmd(t *testing.T) {
-	_ = t
-	// envDir := "./testdata/env"
-	// command := "/bin/bash ./testdata/echo.sh"
-	// args := []string{"arg1=1", "arg2=2"}
+	defer os.Remove("go-envdir")
 
-	// result, err := RunCommand(envDir, command, args...)
+	envDir := "./testdata/env"
+	command := "/bin/bash ./testdata/echo.sh"
+	args := []string{"arg1=1", "arg2=2"}
+	cmdArgs := append([]string{envDir, command}, args...)
+
+	cmd := exec.Command("./go-envdir", cmdArgs...)
+	cmd.Env = append(os.Environ(), "ADDED=from original env")
+
+	result, err := cmd.CombinedOutput()
+	if err != nil {
+		t.Fatalf("Failed to run go-envdir: %v", err)
+	}
+
+	// Define the expected output
+	expected := `HELLO is ("hello")
+BAR is (bar)
+FOO is (   foo
+with new line)
+UNSET is ()
+ADDED is (from original env)
+EMPTY is ()
+arguments are arg1=1 arg2=2`
+
+	// Compare the result with the expected output
+	if strings.TrimSpace(string(result)) != expected {
+		t.Errorf("Invalid output: %s\nExpected: %s", result, expected)
+	}
+}
+
+func init() {
+	args := []string{"build", "-o", "go-envdir"}
+	cmd := exec.Command("go", args...)
+	result, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println("building failed", string(result), err)
+	}
 }
