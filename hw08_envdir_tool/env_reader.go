ae679fdec4d64c106e5c4890ce9b7ab6e3a8ac10
ksa75 <kirillov-sa@yandex.ru>
2025-04-06 13:28:35 +0300

    HW8 is completed

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
 type Environment map[string]EnvValue
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
@@ -11,6 +18,40 @@ type EnvValue struct {
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	env := make(Environment)
+
+	files, err := os.ReadDir(dir)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read dir: %s, %v", dir, err)
+	}
+
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
+
+		fPath := dir + "/" + file.Name()
+		f, err := os.Open(fPath)
+		if err != nil {
+			return nil, fmt.Errorf("failed to open file: %s, %v", fPath, err)
+		}
+		defer f.Close()
+
+		// Read the first line from the file
+		scanner := bufio.NewScanner(f)
+		if scanner.Scan() {
+			value := strings.TrimSpace(scanner.Text())
+			if value == "" {
+				// If the file is empty, mark for removal
+				env[file.Name()] = EnvValue{NeedRemove: true}
+			} else {
+				// Otherwise, store the value
+				env[file.Name()] = EnvValue{Value: value}
+			}
+		} else if err := scanner.Err(); err != nil {
+			return nil, fmt.Errorf("error reading file %s: %v", fPath, err)
+		}
+	}
+
+	return env, nil
 }
--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,68 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"runtime"
+	"syscall"
+)
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
-	return
+	// Prepare environment variables
+	for key, value := range env {
+		if value.NeedRemove {
+			// If NeedRemove is true, unset the variable
+			if runtime.GOOS == "windows" {
+				// On Windows, use os.Unsetenv
+				os.Unsetenv(key)
+			} else {
+				// On Linux and other OSs, use syscall.Unsetenv
+				syscall.Unsetenv(key)
+			}
+		} else {
+			// Otherwise, set the environment variable
+			if runtime.GOOS == "windows" {
+				// On Windows, use os.Setenv
+				os.Setenv(key, value.Value)
+			} else {
+				// On Linux and other OSs, use syscall.Setenv
+				syscall.Setenv(key, value.Value)
+			}
+		}
+	}
+
+	// Prepare the command
+	command := cmd[0]
+	args := cmd[1:]
+
+	// If on Windows, prepend cmd.exe /C to run the command in a shell
+	if runtime.GOOS == "windows" {
+		command = "cmd.exe"
+		args = append([]string{"/C"}, cmd...)
+	}
+
+	// Create a new command with the given arguments
+	cmdExec := exec.Command(command, args...)
+
+	// Redirect standard I/O to the parent process
+	cmdExec.Stdout = os.Stdout
+	cmdExec.Stderr = os.Stderr
+	cmdExec.Stdin = os.Stdin
+
+	// Run the command
+	err := cmdExec.Run()
+	if err != nil {
+		// If an error occurs, capture the exit code
+		if exitError, ok := err.(*exec.ExitError); ok {
+			return exitError.ExitCode()
+		} else {
+			fmt.Printf("Error executing command: %v\n", err)
+			return 1
+		}
+	}
+
+	// If successful, return 0 exit code
+	return 0
 }
