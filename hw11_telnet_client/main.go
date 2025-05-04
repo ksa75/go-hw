801f377fc06093503b9446aa8c30c526b6952734
ksa75 <kirillov-sa@yandex.ru>
2025-05-04 16:47:05 +0300

    HW11 is completed

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,70 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+var timeout time.Duration
+
+func init() {
+	flag.DurationVar(&timeout, "timeout", 10*time.Second, "connection timeout")
+}
+
 func main() {
-	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	flag.Parse()
+
+	if flag.NArg() != 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s [--timeout=10s] host port\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	address := flag.Arg(0) + ":" + flag.Arg(1)
+	client := NewTelnetClient(address, timeout, os.Stdin, os.Stdout)
+
+	if err := client.Connect(); err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to connect: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Fprintf(os.Stderr, "Connected to %s\n", address)
+
+	// Handle Ctrl+C (SIGINT)
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGINT)
+
+	done := make(chan struct{})
+
+	// Read from stdin and send to server
+	go func() {
+		err := client.Send()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Send error: %v\n", err)
+		}
+		fmt.Fprintln(os.Stderr, "EOF received (Ctrl+D). Closing connection.")
+		done <- struct{}{}
+	}()
+
+	// Read from server and write to stdout
+	go func() {
+		err := client.Receive()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Receive error: %v\n", err)
+		}
+		fmt.Fprintln(os.Stderr, "Connection closed by remote host.")
+		done <- struct{}{}
+	}()
+
+	// Wait for either Ctrl+C or EOF/socket close
+	select {
+	case <-sigCh:
+		fmt.Fprintln(os.Stderr, "\nReceived SIGINT, exiting.")
+	case <-done:
+		// already logged
+	}
+
+	client.Close()
 }
