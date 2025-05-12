cac95194c489dedb7435ca7f30cd8198a1c0141e
ksa75 <kirillov-sa@yandex.ru>
2025-05-12 09:46:39 +0300

    Hw12_13_14_15_16_calendar is completed

---
+++ hw12_13_14_15_16_calendar/internal/config/config.go
@@ -0,0 +1,36 @@
+package main
+
+import "github.com/BurntSushi/toml"
+
+func Read(fpath string) (c Config, err error) {
+	_, err = toml.DecodeFile(fpath, &c)
+	return
+}
+
+type Config struct {
+	PSQL PSQLConfig
+}
+
+type PSQLConfig struct {
+	DSN       string
+	Migration string
+}
+
+// При желании конфигурацию можно вынести в internal/config.
+// Организация конфига в main принуждает нас сужать API компонентов, использовать
+// при их конструировании только необходимые параметры, а также уменьшает вероятность циклической зависимости.
+// type Config struct {
+// 	Logger LoggerConf
+// 	// TODO
+// }
+
+// type LoggerConf struct {
+// 	Level string
+// 	// TODO
+// }
+
+// func NewConfig() Config {
+// 	return Config{}
+// }
+
+// TODO
---
+++ hw12_13_14_15_16_calendar/main.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	// "github.com/fixme_my_friend/hw12_13_14_15_calendar/internal/app"
+	// "github.com/fixme_my_friend/hw12_13_14_15_calendar/internal/logger"
+	// internalhttp "github.com/fixme_my_friend/hw12_13_14_15_calendar/internal/server/http"
+	// memorystorage "github.com/fixme_my_friend/hw12_13_14_15_calendar/internal/storage/memory"
+
+////////////////////////////////
+	// "github.com/OtusGolang/webinars_practical_part/25-sql/internal/app"
+	"github.com/ksa75/go-hw/hw12_13_14_15_16_calendar/internal/app"
+	// "github.com/OtusGolang/webinars_practical_part/25-sql/internal/config"
+    "github.com/ksa75/go-hw/hw12_13_14_15_16_calendar/internal/config"
+	"github.com/ksa75/go-hw/hw12_13_14_15_16_calendar/internal/storage/sql"
+	// "github.com/OtusGolang/webinars_practical_part/25-sql/internal/repository/psql"
+)
+
+var configFile string
+
+func init() {
+	flag.StringVar(&configFile, "config", "/etc/calendar/config.toml", "Path to configuration file")
+}
+
+func main() {
+	flag.Parse()
+
+	if flag.Arg(0) == "version" {
+		printVersion()
+		return
+	}
+////////////////////////
+	if err := mainImpl(); err != nil {
+		log.Fatal(err)
+	}
+////////////////////////
+	// config := NewConfig()
+	// logg := logger.New(config.Logger.Level)
+
+	// storage := memorystorage.New()
+	// calendar := app.New(logg, storage)
+
+	// server := internalhttp.NewServer(logg, calendar)
+
+	// ctx, cancel := signal.NotifyContext(context.Background(),
+	// 	syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP)
+	// defer cancel()
+
+	// go func() {
+	// 	<-ctx.Done()
+
+	// 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+	// 	defer cancel()
+
+	// 	if err := server.Stop(ctx); err != nil {
+	// 		logg.Error("failed to stop http server: " + err.Error())
+	// 	}
+	// }()
+
+	// logg.Info("calendar is running...")
+
+	// if err := server.Start(ctx); err != nil {
+	// 	logg.Error("failed to start http server: " + err.Error())
+	// 	cancel()
+	// 	os.Exit(1) //nolint:gocritic
+	// }
+}
+
+
+func mainImpl() error {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	c, err := config.Read("configs/local.toml")
+	if err != nil {
+		return fmt.Errorf("cannot read config: %v", err)
+	}
+
+	r := new(sql.Storage)
+	if err := r.Connect(ctx, c.PSQL.DSN); err != nil {
+		return fmt.Errorf("cannot connect to psql: %v", err)
+	}
+	defer func() {
+		if err := r.Close(); err != nil {
+			log.Println("cannot close psql connection", err)
+		}
+	}()
+
+	if err := r.Migrate(ctx, c.PSQL.Migration); err != nil {
+		return fmt.Errorf("cannot migrate: %v", err)
+	}
+
+	a, err := app.New(r)
+	if err != nil {
+		return fmt.Errorf("cannot create app: %v", err)
+	}
+
+	if err := a.Run(ctx); err != nil {
+		return fmt.Errorf("cannot run app: %v", err)
+	}
+
+	return nil
+}
